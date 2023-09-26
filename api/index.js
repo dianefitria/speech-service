856a20bf55473a77c9f19cff614e8dd1aa23583c
Diane Fitria <diane.fitria@gmail.com>
2023-09-26 14:59:38 +0700

    audio transcription

--- api/index.js
+++ api/index.js
@@ -1,16 +1,77 @@
-const app = require('express')();
-const { v4 } = require('uuid');
+import express from 'express';
+import axios, { post } from 'axios';
+import { createWriteStream, createReadStream, unlinkSync } from 'fs';
+import { join } from 'path';
+
+const app = express();
+
+app.use(express.json());
 
 app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  res.end('Hello!');
 });
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+app.post('/api/audio/transcriptions', async (req, res) => {
+
+  const audioUrl = req.body.url;
+  const apiUrl = 'https://api.openai.com/v1/audio/transcriptions';
+  const payload = {
+    model: 'whisper-1',
+  };
+
+  const response = await audioTranscription(audioUrl, apiUrl, payload);
+
+  res.send({
+    data: response,
+  })
 });
 
-module.exports = app;
+async function downloadFile(url, destinationPath) {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: url,
+      responseType: 'stream',
+    });
+
+    const writer = createWriteStream(destinationPath);
+
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+      writer.on('finish', resolve);
+      writer.on('error', reject);
+    });
+  } catch (error) {
+    throw new Error(`Error downloading file: ${error.message}`);
+  }
+}
+
+async function audioTranscription(audioUrl, apiUrl, payload) {
+  const tempFilePath = join(__dirname, 'tempfile.txt');
+
+  try {
+    await downloadFile(audioUrl, tempFilePath);
+    
+    const formData = new FormData();
+    formData.append('file', createReadStream(tempFilePath));
+    for (const key in payload) {
+      formData.append(key, payload[key]);
+    }
+
+    const response = await post(apiUrl, formData, {
+      headers: {
+        'Authorization': 'Bearer ' + process.env.OPENAI_API_KEY,
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    unlinkSync(tempFilePath);
+
+    return response.data;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+export default app;
